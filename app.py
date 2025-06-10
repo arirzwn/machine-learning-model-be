a46aae6331a4a0f1190e21a192ab55247ba9839f
arirzwn <aririzwan0@gmail.com>
2025-06-11 01:54:08 +0700

    chore: comment out main execution block in app.py

--- app.py
+++ app.py
@@ -41,6 +41,6 @@ def predict():
 
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(debug=False, host='0.0.0.0', port=port)
